72e864f5a095c11cc2f94a489dd23ead33114a47
Trevor Yilk <tayilk@gmail.com>
2021-04-08 10:41:28 -0500

    Add files via upload

---
+++ GPOSearchString.ps1
@@ -0,0 +1,14 @@
+﻿$string = "ALLCARE2008"
+
+$GPOs = Get-GPO -all
+
+foreach ($gpo in $GPos) {
+    $report = Get-GPOReport -Guid $gpo.Id -ReportType xml
+    if ($report -match $string){
+        write-host "Match Found: $($gpo.DisplayName)"
+    }
+    else{
+        Write-Host "No Match in: $($gpo.DisplayName)"
+    }
+
+}
